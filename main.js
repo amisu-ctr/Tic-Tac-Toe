1b188e1b08bb8ba43cb3344b56028198872f82f6
amisu-ctr <hamisurahman@gmail.com>
2023-04-04 14:10:59 +0000

    Listening for clicks on each created square

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const Gameboard = (() => {
     let gameboard = ['','','','','','','','','']
 
+
+    //set variable to an empty . loop through gameboard array . upon each iteration create a string and attach it to the boardHtml variable
+    //after the loop is done . grab the gameboard element  div from the html and set its value to the boardHtml 
+    //this will populate it child divs with based on the number of items in the gameboard array 
     const render = () => {
         let boardHTML = '';
 
-        gameboard.forEach((square, index) => {
+        gameboard.forEach((square, index) => {  
             boardHTML += `<div class="square" id="square-${index}">${square} </div>`
         })
-        document.querySelector("#gameboard").innerHTML = boardHTML;
+        document.querySelector("#gameboard").innerHTML = boardHTML; 
+        const squares = document.querySelectorAll(".square") //select all created div element with class square
+        squares.forEach((square) => {
+            square.addEventListener('click', Game.handleClick)
+        })
+        
     }
 
     return {
@@ -15,13 +24,13 @@ const Gameboard = (() => {
     }
 })()
 
+
 const createPlayer = (name, mark) => {
     return {
         name,
         mark
     }
 }
-
 
 const Game = (() => {
     let players = [];
@@ -43,5 +52,5 @@ const Game = (() => {
 
 const startButton = document.querySelector("#start-button");
 startButton.addEventListener("click", () => {
-    console.log("hamisu")
+    Game.start();
 })
